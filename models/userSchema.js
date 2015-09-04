1a1cc0ae47b539f45d8b50c02f63d8a192075cf9
JScearcy <jacobscearcy@gmail.com>
2015-09-04 15:19:06 -0500

    App and login form works

---
+++ models/userSchema.js
@@ -0,0 +1,35 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    bcrypt = require('bcrypt'),
+    SALT_WORK_FACTOR = 10;
+
+var UserSchema = new Schema({
+  username: {type: String, required: true, index: {unique: true}},
+  password: {type: String, required: true},
+  firstname: String,
+  lastname: String,
+  email: String
+});
+UserSchema.pre('save', function(next){
+  var user = this;
+  if(!user.password.isModified()){
+    return next();
+  }
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt){
+    if (err) return next(err);
+    bcrypt.hash(user.password, salt, function(err, hash){
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+UserSchema.methods.verifyPassword = function(candidatePassword, callback){
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch){
+    if (err) return callback(err);
+    callback(null, isMatch);
+  })
+};
+
+module.exports = mongoose.model('User', UserSchema);
